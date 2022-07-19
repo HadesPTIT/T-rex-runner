94830dc0f3b1624949cdda225d474843f31db0e5
HuongPT <huongpt45@onemount.com>
2022-07-19 21:44:15 +0700

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# t_rex_flame
+
+A new Flutter project.
+
+## Getting Started
+
+https://docs.flame-engine.org/1.2.0/
+
---
+++ android/app/src/main/kotlin/com/example/t_rex_flame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.t_rex_flame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
